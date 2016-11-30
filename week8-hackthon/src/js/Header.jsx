ed6a5177ca2d0ee12b4211f8ae4a91d22b86f5bd
danny8508021 <dannywang0802@yahoo.com.tw>
2016-11-30 16:35:22 +0800

    Complete head bar

--- week8-hackthon/src/js/Header.jsx
+++ week8-hackthon/src/js/Header.jsx
@@ -8,7 +8,19 @@ class Header extends React.Component {
 
   render() {
     return (
-      <div className="header"> {this.props.children} </div>
+      <nav id="head-bar" className="navbar navbar-inverse navbar-static-top">
+          <div className="navbar-header">
+            <a id="brand" className="navbar-brand" href="/">{this.props.children}</a>
+          </div>
+          <div>
+            <ul className="nav navbar-nav">
+              <li><a href="/"> exam </a></li>
+              <li><a href="/"> slide </a></li>
+              <li><a href="/"> e-Book </a></li>
+              <li><a href="/"> note </a></li>
+            </ul>
+          </div>
+      </nav>
     );
   }
 }
