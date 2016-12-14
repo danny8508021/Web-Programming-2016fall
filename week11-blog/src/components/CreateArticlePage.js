0f728d1b690cc8db04c6bea97c07750305215017
danny8508021 <dannywang0802@yahoo.com.tw>
2016-12-14 22:29:59 +0800

    complete create article

--- week11-blog/src/components/CreateArticlePage.js
+++ week11-blog/src/components/CreateArticlePage.js
@@ -1,5 +1,6 @@
 import 'isomorphic-fetch';
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 // 'react-quill'
 // 'react-tagsinput'
@@ -18,13 +19,30 @@ class CreateArticlePage extends Component {
     const confirm = window.confirm('確定要新增文章嗎？');
     if (confirm) {
       // fetch here
-      const body = this.state;
-      fetch('/api/articles', {
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        }
-      })
+      const my = this;
+      
+      this.setState({
+        title: this.refs.titleName.value,
+        content: this.refs.articleContent.value,
+        tags: [this.refs.tagsName.value],
+      }, () => {
+        console.log(my.state);
+        fetch('/api/articles', {
+          method: 'post',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(my.state),
+        })
+        .then(res => { return res.json(); })
+        .then(json => {
+          my.refs.titleName.value = '';
+          my.refs.articleContent.value = '';
+          my.refs.tagsName.value = '';
+          console.log('reponse from server: ', json);
+        });
+      });
     }
   }
 
@@ -43,16 +61,28 @@ class CreateArticlePage extends Component {
         <div className="row">
           <div className="col-md-12">
             {/* title */}
+            <form action="">
+              <label htmlFor="title-name">標題:</label>
+              <input id="title-name" ref="titleName" type="text"/>
+            </form>
           </div>
         </div>
         <div className="row">
           <div className="col-md-12">
             {/* tags */}
+            <form action="">
+              <label htmlFor="tags-name">標籤:</label>
+              <input id="tags-name" ref="tagsName" type="text"/>
+            </form>
           </div>
         </div>
         <div className="row">
           <div className="col-md-12">
             {/* content */}
+            <form action="">
+              <label htmlFor="artile-content">文章內容:</label>
+              <input id="article-content" ref="articleContent" type="text"/>
+            </form>
           </div>
         </div>
       </div>
